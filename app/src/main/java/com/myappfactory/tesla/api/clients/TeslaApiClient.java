2c6cc7cea839a5e63842c0e712bcda194ca4fed4
Praneet Loke <praneetloke@hotmail.com>
2014-07-28 00:16:56 -0400

    Don\'t want it.

--- app/src/main/java/com/myappfactory/tesla/api/clients/TeslaApiClient.java
+++
@@ -1,9 +0,0 @@
-package com.myappfactory.tesla.api.clients;
-
-/**
- * Created by ploke on 7/27/14.
- */
-public interface TeslaApiClient {
-    void setCookie(String name, String value);
-    String getCookie(String name);
-}
